ea112d8a3b7cac3d7c2c117cf6bf21fc4f1dc812
o-tofu <81277991+o-tofu7741@users.noreply.github.com>
2023-11-10 04:37:29 +0900

    Studentクラス実装

--- Util/student.py
+++ Util/student.py
@@ -1,24 +1,57 @@
 import glob
 from os import path
 
-from Util.answer import Answer
+from answer import Answer
 
 
 class Student:
-    def __init__(self, student_dir_path, tasks: list[dict[str]]) -> None:
-        self.dir_path = student_dir_path
-        # self.correct_path_list = correct_file_list
-        self.answer_path_list = glob.glob(path.join(student_dir_path, "*"))
+    def __init__(self, student_dir_path: str, tasks: list[dict[str, str]]) -> None:
+        self.dir_path = student_dir_path.replace("\\", "/")
+        self.user = path.basename(self.dir_path)
         self.answers: list[Answer] = []
         self.tasks = tasks
-
-    def __str__(self) -> str:
-        return self.dir_path
+        self.not_exists_tasks: list[str] = []
+        self.result: str = ""
 
     def set_answers(self):
+        ans_abs_paths = glob.glob(path.join(self.dir_path, "*"))
+        ans_names = list(map(path.basename, ans_abs_paths))
         for task in self.tasks:
-            print(task)
+            if task["name"] in ans_names:
+                self.answers.append(
+                    Answer(
+                        path.join(self.dir_path, task["name"]),
+                        task.get("args", ""),
+                        task.get("input"),
+                    )
+                )
+            else:
+                self.not_exists_tasks.append(task["name"])
+
+    def collect_results(self):
+        self.result += (
+            f"{' USER : ' + self.user + ' ':#^70}\n\n"
+            f"左記の課題ファイル無し or 名前ミス : {' ,'.join(self.not_exists_tasks) if self.not_exists_tasks else 'ミスしているファイルはありません'}\n"
+        )
+
+        for ans in self.answers:
+            ans.get_code()
+            ans.execute()
+            self.result += (
+                f"{' 課題 : ' + ans.task_name + ' ':=^70}\n\n"
+                f"FILE LIST : {ans.file_list}\n\n"
+                f"{' コード ':-^70}\n\n"
+                f"{ans.code_txt}\n"
+                f"{' 実行結果 ' + ans.task_name + ' ':~^70}\n"
+                f"{ans.result_txt}\n\n"
+            )
 
 
 if __name__ == "__main__":
-    Student("hoge")
+    hoge = Student(
+        "C:/github/auto-scoring-python/tmp/20J5-129@20j5129",
+        [{"name": "Expand.java"}, {"name": "Loop.java"}],
+    )
+    hoge.set_answers()
+    hoge.collect_results()
+    print(hoge.result)
